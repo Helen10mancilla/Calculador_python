ee0fe2e5d117eae9d590e0cd311090798926afb5
KarlaMartinez0 <amartinezalvarez6@gmail.com>
2026-09-03 20:43:25 -0500

    Funcion resta

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+def restar(a, b):
+    return a - b
+
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+resultado = restar(num1, num2)
+
+print(f"La resta de {num1} - {num2} es {resultado}")
